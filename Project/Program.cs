a48ac7f7f84b11936ab47e88d8e0d6d56c610d2e
Andreas Nägeli <andr.naegeli@gmail.com>
2019-04-13 18:15:10 +0200

    Add docker features

--- Project/Program.cs
+++ Project/Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
 
 using GenHTTP.Core;
-
 using GenHTTP.Website.Utilities;
 using Project.Utilities;
 
@@ -15,23 +15,38 @@ namespace GenHTTP.Website
     public static class Program
     {
 
-        public static void Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            var project = Project.Create();
-
-            var server = Server.Create()
-                               .Router(project)
-                               .Compression(new BrotliCompression())
-                               .Extension(new CacheExtension())
-                               .Console();
-
-            using (var instance = server.Build())
+            try
             {
-                Console.WriteLine("Press any key to stop ...");
-                Console.ReadLine();
+                var project = Project.Create();
+
+                var server = Server.Create()
+                                   .Router(project)
+                                   .Compression(new BrotliCompression())
+                                   .Extension(new CacheExtension())
+                                   .Console();
+
+                using (var instance = server.Build())
+                {
+                    Console.WriteLine("Running ...");
+
+#if DEBUG
+                    Console.ReadLine();
+#else
+                await Task.Run(() => Thread.Sleep(Timeout.Infinite));
+#endif
+                }
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return -1;
             }
         }
-        
+
     }
 
 }
